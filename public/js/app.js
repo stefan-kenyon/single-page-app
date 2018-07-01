cd5019a91089bebb11d26e5d76890016dd7a4192
Stefan <stefan.kenyon@gmail.com>
2018-06-30 22:19:17 -0500

    =30JUN fixing bugs

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
-import axios from "axios";
-
 window.addEventListener('load', () => {
+
    const el = $('#app');
 
    // Compile Handlebar Templates
+
    const errorTemplate = Handlebars.compile($('#error-template').html());
    const ratesTemplate = Handlebars.compile($('#rates-template').html());
    const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
@@ -18,100 +18,44 @@ window.addEventListener('load', () => {
             title: 'Error 404 - Page NOT Found!',
             message: `The path '/${path}' does not exist on this site`,
          });
-      el.html(html);
+         el.html(html);
       },
    });
 
    // Instantiate api handler
-   const api = axios.create({
-      baseURL: 'http://localhost:3000/api/',
-      timeout: 5000,
+const api = axios.create({
+   baseURL: 'http://localhost:3000/api',
+   timeout: 5000,
    });
-
-   //Display Error Banner
+   // Display Error Banner
    const showError = (error) => {
-      const {title, message} = error.response.data;
-      const html = errorTemplate({color: 'red', title, message});
-      el.html(html);
+   const { title, message } = error.response.data;
+   const html = errorTemplate({ color: 'red', title, message });
+   el.html(html);
    };
-
    'red'// Display Latest Currency Rates
-   router.add('/', async() => {
-      //Display loader first
-      let html = ratesTemplate();
-      el.html(html);
-      try {
-         // Load Currency Rates
-         const response = await api.get('/rates');
-         const {base, date, rates} = resonse.data;
-         // Di'/rates/'// Display Rates Table
-         html = ratesTemplate({base, date, rates});
-         el.html(html);
-      } catch (error) {
-         showError(error);
-      } finally {
-         // Remove loader status
-         $('.loading').removeClass('loading');
-      }
+   router.add('/', async () => {
+   // Display loader first
+   let html = ratesTemplate();
+   el.html(html);
+   try {
+   // Load Currency Ratesconst response = await api.get('/rates');
+   const { base, date, rates } = response.data;
+   // Di'/rates'// Display Rates Table
+   html = ratesTemplate({ base, date, rates });
+   el.html(html);
+   } catch (error) {
+   showError(error);
+   } finally {
+   // Remove loader status
+   $('.loading').removeClass('loading');
+   }
    });
+   
 
-   // Perform POST request, calculate and display conversion results
-   const getConversionResults = async () => {
-      // Extra form data
-      const from = $('#from').val();
-      const to = $('#to').val();
-      const amount = $('#amount').val();
-      // Send post data to Express(proxy) server
-      try {
-         const response = await api.post('/convert', {from, to});
-         const {rate} = response.data;
-         const result = rate * amount;
-         $('#result').html(`${to} ${result}`);
-      } catch (error) {
-         showError(error);
-      } finally {
-         $('#result-segment').removeClass('loading');
-      }
-   };
-
-   // Handle Convert Button Click Event
-   const convertRatesHandler = () => {
-      if($('.ui.form').form('is valid')) {
-         // hide error message
-         $('.ui.error.message').hide();
-         // Post to Express server
-         $('#result-segment').addClass('loading');
-         getConversionResults();
-         // Prevent page from submitting to server
-         return false;
-      }
-      return true;
-   };
-
-   router.add('/exchange', async () => {
-      // Display loader first
+   router.add('/exchange', () => {
       let html = exchangeTemplate();
       el.html(html);
-      try {
-         // Load Symbols
-         const response = await api.get('/symbols');
-         const {symbols} = response.data;
-         html = exchangeTemplate({symbols});
-         el.html(html);
-         $('.loading').removeClass('loading');
-         // Validate Form Inputs
-         $('.ui.form').form({
-            fields: {
-               from: 'empty',
-               to: 'empty', 
-               amount: 'decimal',
-            },
-         });
-         // Specify Submit Handler
-         $('.submit').click(convertRatesHandler);
-      } catch (error) {
-         showError(error);
-      }
    });
 
    router.add('/historical', () => {
@@ -122,17 +66,15 @@ window.addEventListener('load', () => {
    // Navigate app to current url
    router.navigateTo(window.location.pathname);
 
-   // Highlight Active Menu on Refresh/Page Reload
+   // Highligh Active Menu on Refresh/Page Reload
    const link = $(`a[href$='${window.location.pathname}']`);
    link.addClass('active');
 
    $('a').on('click', (e) => {
-
-      // Block browser page load
       e.preventDefault();
 
       // Highlight Active Menu on Click
-      const target = $(event.target);
+      const target = $(e.target);
       $('.item').removeClass('active');
       target.addClass('active');
 
@@ -141,5 +83,148 @@ window.addEventListener('load', () => {
       const path = href.substr(href.lastIndexOf('/'));
       router.navigateTo(path);
    });
+   
+   // import axios from "axios";
+
+   // const el = $('#app');
+
+   // // Compile Handlebar Templates
+   // const errorTemplate = Handlebars.compile($('#error-template').html());
+   // const ratesTemplate = Handlebars.compile($('#rates-template').html());
+   // const exchangeTemplate = Handlebars.compile($('#exchange-template').html());
+   // const historicalTemplate = Handlebars.compile($('#historical-template').html());
+
+   // // Router Declaration
+   // const router = new Router({
+   //    mode: 'history',
+   //    page404: (path) => {
+   //       const html = errorTemplate({
+   //          color: 'yellow',
+   //          title: 'Error 404 - Page NOT Found!',
+   //          message: `The path '/${path}' does not exist on this site`,
+   //       });
+   //    el.html(html);
+   //    },
+   // });
+
+   // // Instantiate api handler
+   // const api = axios.create({
+   //    baseURL: 'http://localhost:3000/api/',
+   //    timeout: 5000,
+   // });
+
+   // //Display Error Banner
+   // const showError = (error) => {
+   //    const {title, message} = error.response.data;
+   //    const html = errorTemplate({color: 'red', title, message});
+   //    el.html(html);
+   // };
+
+   // 'red'// Display Latest Currency Rates
+   // router.add('/', async() => {
+   //    //Display loader first
+   //    let html = ratesTemplate();
+   //    el.html(html);
+   //    try {
+   //       // Load Currency Rates
+   //       const response = await api.get('/rates');
+   //       const {base, date, rates} = resonse.data;
+   //       // Di'/rates/'// Display Rates Table
+   //       html = ratesTemplate({base, date, rates});
+   //       el.html(html);
+   //    } catch (error) {
+   //       showError(error);
+   //    } finally {
+   //       // Remove loader status
+   //       $('.loading').removeClass('loading');
+   //    }
+   // });
+
+   // // Perform POST request, calculate and display conversion results
+   // const getConversionResults = async () => {
+   //    // Extra form data
+   //    const from = $('#from').val();
+   //    const to = $('#to').val();
+   //    const amount = $('#amount').val();
+   //    // Send post data to Express(proxy) server
+   //    try {
+   //       const response = await api.post('/convert', {from, to});
+   //       const {rate} = response.data;
+   //       const result = rate * amount;
+   //       $('#result').html(`${to} ${result}`);
+   //    } catch (error) {
+   //       showError(error);
+   //    } finally {
+   //       $('#result-segment').removeClass('loading');
+   //    }
+   // };
+
+   // // Handle Convert Button Click Event
+   // const convertRatesHandler = () => {
+   //    if($('.ui.form').form('is valid')) {
+   //       // hide error message
+   //       $('.ui.error.message').hide();
+   //       // Post to Express server
+   //       $('#result-segment').addClass('loading');
+   //       getConversionResults();
+   //       // Prevent page from submitting to server
+   //       return false;
+   //    }
+   //    return true;
+   // };
+
+   // router.add('/exchange', async () => {
+   //    // Display loader first
+   //    let html = exchangeTemplate();
+   //    el.html(html);
+   //    try {
+   //       // Load Symbols
+   //       const response = await api.get('/symbols');
+   //       const {symbols} = response.data;
+   //       html = exchangeTemplate({symbols});
+   //       el.html(html);
+   //       $('.loading').removeClass('loading');
+   //       // Validate Form Inputs
+   //       $('.ui.form').form({
+   //          fields: {
+   //             from: 'empty',
+   //             to: 'empty', 
+   //             amount: 'decimal',
+   //          },
+   //       });
+   //       // Specify Submit Handler
+   //       $('.submit').click(convertRatesHandler);
+   //    } catch (error) {
+   //       showError(error);
+   //    }
+   // });
+
+   // router.add('/historical', () => {
+   //    let html = historicalTemplate();
+   //    el.html(html);
+   // });
+
+   // // Navigate app to current url
+   // router.navigateTo(window.location.pathname);
+
+   // // Highlight Active Menu on Refresh/Page Reload
+   // const link = $(`a[href$='${window.location.pathname}']`);
+   // link.addClass('active');
+
+   // $('a').on('click', (e) => {
+
+   //    // Block browser page load
+   //    e.preventDefault();
+
+   //    // Highlight Active Menu on Click
+   //    const target = $(event.target);
+   //    $('.item').removeClass('active');
+   //    target.addClass('active');
+
+   //    // Navigate to clicked url
+   //    const href = target.attr('href');
+   //    const path = href.substr(href.lastIndexOf('/'));
+   //    router.navigateTo(path);
+   // });
 
 });
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-require('dotenv').config(); //read .env files
+require('dotenv').config(); // read .env files
 const express = require('express');
-const {getRates, getSymbols} = require('./lib/fixer-service');
-const {convertCurrency} = require('./lib/free-currency-service');
+const {getRates} = require('./lib/fixer-service');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,76 +9,131 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 // Allow front-end access to node_modules folder
-app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+app.use('/scripts', express.static(`${__dirname}/node_modules`));
 
 // Express Error handler
 const errorHandler = (err, req, res) => {
    if(err.response) {
-      // Request was made and response was not in 2xx
-      res.status(403).send({ title: 'Server responded with an error', message: err.message});
-   } else if(err.request) {
-      // Request was made but no response was received
-      res.status(503).send({ title: 'Unable to communicate with server', message: err.message});
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      res.status(403).send({title: 'Server responded with an error', message: err.message});
+   } else if (err.request) {
+      // The request was made by no response was received
+      res.status(503).send({title: 'Unable to communicate with server', message: err.message});
    } else {
       // Something happened in setting up the request that triggered an Error
-      res.status(500).send({title: 'An unexpected error occurred', message: err.message});
+      res.status(500).send({title: 'An unexpected error occured', message: err.message});
    }
 };
 
 // Fetch Latest Currency Rates
-app.get('/api/rates', async(req, res) => {
+app.get('/api/rates', async (req, res) => {
    try {
       const data = await getRates();
       res.setHeader('Content-Type', 'application/json');
       res.send(data);
    } catch (error) {
-      errorHandler(error, res, req);
-   }
-});
-
-// Fetch Symbols
-app.get('/api/symbols', async (req, res) => {
-   try {
-      const data = await getSymbols();
-      res.setHeader('Content-Type', 'application/json');
-      res.send(data);
-   } catch (error) {
       errorHandler(error, req, res);
    }
 });
 
-// Convert Currency
-app.post('/api/convert', async (req, res) => {
-   try {
-      const {from, to} = req.body;
-      const data = await convertCurrency(from, to);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(data);
-   } catch (error) {
-      errorHandler(error, req, res);
-   }
-});
-
+// Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
-
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
-   console.log('Listening on %d', port);
+   console.log('listening on %d', port);
 });
 
-// const test = async() => {
-//    const data = await getRates();
-//    console.log(data);
-// }
+const test = async() => {
+   const data = await getRates();
+   console.log(data);
+}
 
-// const test = async() => {
-//    const data = await getSymbols();
-//    console.log(data);
-// }
+test();
 
-// const test = async() => {
-//    const data = await convertCurrency('USD', 'KES');
-//    console.log(data);
-// }
 
-// test();
+// require('dotenv').config(); //read .env files
+// const express = require('express');
+// const {getRates, getSymbols} = require('./lib/fixer-service');
+// const {convertCurrency} = require('./lib/free-currency-service');
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// // Set public folder as root
+// app.use(express.static('public'));
+
+// // Allow front-end access to node_modules folder
+// app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+// // Express Error handler
+// const errorHandler = (err, req, res) => {
+//    if(err.response) {
+//       // Request was made and response was not in 2xx
+//       res.status(403).send({ title: 'Server responded with an error', message: err.message});
+//    } else if(err.request) {
+//       // Request was made but no response was received
+//       res.status(503).send({ title: 'Unable to communicate with server', message: err.message});
+//    } else {
+//       // Something happened in setting up the request that triggered an Error
+//       res.status(500).send({title: 'An unexpected error occurred', message: err.message});
+//    }
+// };
+
+// // Fetch Latest Currency Rates
+// app.get('/api/rates', async(req, res) => {
+//    try {
+//       const data = await getRates();
+//       res.setHeader('Content-Type', 'application/json');
+//       res.send(data);
+//    } catch (error) {
+//       errorHandler(error, res, req);
+//    }
+// });
+
+// // Fetch Symbols
+// app.get('/api/symbols', async (req, res) => {
+//    try {
+//       const data = await getSymbols();
+//       res.setHeader('Content-Type', 'application/json');
+//       res.send(data);
+//    } catch (error) {
+//       errorHandler(error, req, res);
+//    }
+// });
+
+// // Convert Currency
+// app.post('/api/convert', async (req, res) => {
+//    try {
+//       const {from, to} = req.body;
+//       const data = await convertCurrency(from, to);
+//       res.setHeader('Content-Type', 'application/json');
+//       res.send(data);
+//    } catch (error) {
+//       errorHandler(error, req, res);
+//    }
+// });
+
+// app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+// // Listen for HTTP requests on port 3000
+// app.listen(port, () => {
+//    console.log('Listening on %d', port);
+// });
+
+// // const test = async() => {
+// //    const data = await getRates();
+// //    console.log(data);
+// // }
+
+// // const test = async() => {
+// //    const data = await getSymbols();
+// //    console.log(data);
+// // }
+
+// // const test = async() => {
+// //    const data = await convertCurrency('USD', 'KES');
+// //    console.log(data);
+// // }
+
+// // test();
